9a67788c1b4c408d6347ceb3196eae0a68b6f988
vetpsk <vetpskulkarni@gmail.com>
2020-12-03 09:59:08 +0100

    first commit

---
+++ CITATION.md
@@ -0,0 +1,3 @@
+Please cite this project as follows:
+
+Pranav Kulkarni (2020),  reproducible workshop - version 0.1. url: github.com/vetpsk/workshop_cc
---
+++ src/example_code_PK.R
@@ -0,0 +1,53 @@
+setwd("/Users/pranavkulkarni/OneDrive - Universiteit Utrecht/Reproducible code workshop")
+
+library(data.table)
+library(dplyr)
+
+df1 <- fread(file = "simulated_df.csv", header = T,
+             nThread = 2)
+
+str(df1)
+summary(df1)
+
+df1$year <- as.character(df1$year)
+df1$HerdIdentifier <- as.factor(df1$HerdIdentifier)
+
+hist(df1$numberculled)
+qqnorm(log(df1$numberculled))
+
+df2 <- as.matrix(df1[,c(6,8,10,12,14,15)])
+cor(df2)
+
+lm1 <- lm(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
+            fatculled + proteinculled + numberheifers + year, data = df1, model = TRUE)
+summary(lm1)
+anova(lm1)
+
+lm2 <- glm(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
+             fatculled + proteinculled + numberheifers + year, data = df1,family = poisson(link = "log"))
+summary(lm2)
+anova(lm2)
+
+library(lme4)
+
+lm3 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
+              fatculled + proteinculled + numberheifers + year + (1|HerdIdentifier), data = df1,)
+summary(lm3)
+anova(lm3)
+
+lm4 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
+              fatculled + proteinculled + numberheifers + (1|HerdIdentifier) + (1|year), data = df1,)
+summary(lm4)
+anova(lm4)
+
+lm5 <- glmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
+               fatculled + proteinculled + numberheifers + (1|HerdIdentifier) + (1|year), data = df1, family = poisson(link = "log"))
+
+summary(lm5)
+anova(lm5)
+
+AIC(lm1)
+AIC(lm2)
+AIC(lm3)
+AIC(lm4)
+AIC(lm5)
