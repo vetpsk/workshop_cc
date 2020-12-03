e031be2bd92796547ff8714c6ee979dc57ad6fd4
vetpsk <vetpskulkarni@gmail.com>
2020-12-03 11:34:41 +0100

    lintr use

--- src/example_code_PK.R
+++ src/example_code_PK.R
@@ -1,10 +1,10 @@
 getwd()
-setwd("/Users/pranavkulkarni/workshop_cc/")
+
 
 library(data.table)
 library(dplyr)
 
-df1 <- fread(file = "data/raw/simulated_df.csv", header = T,
+df1 <- fread(file = "./data/raw/simulated_df.csv", header = T,
              nThread = 2)
 
 str(df1)
@@ -21,33 +21,39 @@ sink()
 hist(df1$numberculled)
 qqnorm(log(df1$numberculled))
 
-df2 <- as.matrix(df1[,c(6,8,10,12,14,15)])
+df2 <- as.matrix(df1[, c(6, 8, 10, 12, 14, 15)])
 cor(df2)
 
-lm1 <- lm(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
-            fatculled + proteinculled + numberheifers + year, data = df1, model = TRUE)
+lm1 <- lm(numberculled ~ lifeprodnculled + numberproductiondaysculled +
+            fatculled + proteinculled + numberheifers +
+            year, data = df1, model = TRUE)
 summary(lm1)
 anova(lm1)
 
-lm2 <- glm(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
-             fatculled + proteinculled + numberheifers + year, data = df1,family = poisson(link = "log"))
+lm2 <- glm(numberculled ~ lifeprodnculled + numberproductiondaysculled +
+             fatculled + proteinculled + numberheifers +
+             year, data = df1, family = poisson(link = "log"))
 summary(lm2)
 anova(lm2)
 
 library(lme4)
 
-lm3 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
-              fatculled + proteinculled + numberheifers + year + (1|HerdIdentifier), data = df1,)
+lm3 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled +
+              fatculled + proteinculled + numberheifers +
+              year + (1 | HerdIdentifier), data = df1, )
 summary(lm3)
 anova(lm3)
 
-lm4 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
-              fatculled + proteinculled + numberheifers + (1|HerdIdentifier) + (1|year), data = df1,)
+lm4 <- lmer(numberculled ~ lifeprodnculled + numberproductiondaysculled +
+              fatculled + proteinculled + numberheifers +
+              (1 | HerdIdentifier) + (1 | year), data = df1)
 summary(lm4)
 anova(lm4)
 
-lm5 <- glmer(numberculled ~ lifeprodnculled + numberproductiondaysculled + 
-               fatculled + proteinculled + numberheifers + (1|HerdIdentifier) + (1|year), data = df1, family = poisson(link = "log"))
+lm5 <- glmer(numberculled ~ lifeprodnculled + numberproductiondaysculled +
+               fatculled + proteinculled + numberheifers +
+               (1 | HerdIdentifier) + (1 | year), data = df1,
+             family = poisson(link = "log"))
 
 summary(lm5)
 anova(lm5)
